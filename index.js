93c906bb1e750fc2136f085982e692f98cb4adf8
chrystian cavalcante <chrystiancavalcante@gmail.com>
2018-02-07 19:30:16 -0200

    make it better

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs')
 
 const init = async() => {
     const connection = await mysql.createConnection({ 
-       connectionLimit : 100,
+       connectionLimit : 10,
        adquirirTimeout : 30000 ,
         host:'us-cdbr-iron-east-05.cleardb.net',
         user:'bac7187b8e72e9',
