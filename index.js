a13e28e6b1cce3b9a6db44450d3d9997a7d7a05c
chrystian cavalcante <chrystiancavalcante@gmail.com>
2018-02-08 06:45:35 -0200

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql2-promise')
+const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const account = require('./account')
