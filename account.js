a64df696af80a4ca9432a3501b498eeab2da51c9
chrystian cavalcante <30943675+chrystiancavalcante@users.noreply.github.com>
2018-02-14 15:51:04 -0200

    Update account.js

--- account.js
+++ account.js
@@ -3,7 +3,7 @@ const app = express.Router()
 const fs = require('fs')
 const crypto = require ('crypto')
 const alg ='aes-256-ctr'
-const pwd = 'abcdefgsssans'
+const pwd = 'abcddcsdscdcd'
 
 
 const init = connection => {
